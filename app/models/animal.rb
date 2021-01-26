03ec5cf45c80afd035de1e9f18a3969b35254600
Nathanstuff <n.odonnell@hotmail.com>
2021-01-26 14:37:30 +1100

    basic animal card styling

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,9 +1,3 @@
 class Animal < ApplicationRecord
     has_many :daily_updates
-
-    after_create :slugify
-
-    def slugify
-        self.slug = id
-    end
 end
--- db/migrate/20210126030423_add_slug_to_animals.rb
+++
@@ -1,5 +0,0 @@
-class AddSlugToAnimals < ActiveRecord::Migration[6.0]
-  def change
-    add_column :animals, :slug, :integer
-  end
-end
