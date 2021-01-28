015a4573fb9935d4e6b8d73fe778320c473caa72
Keiran <keiranhoward.dev@gmail.com>
2021-01-29 09:29:20 +1100

    adding react hero

--- app/javascript/components/hero/hero.jsx
+++ app/javascript/components/hero/hero.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './hero.scss';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+
 
 const Hero = () => {
-  return <div>Hero Image</div>;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <Typography component="div" className="hero" style={ 'height: 100vh; width: 100%; background-color: black; background-repeat: no-repeat; background-attachment: fixed; background-size: cover; display: flex; justify-content: center; min-height: 600px;'} />
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default Hero;
