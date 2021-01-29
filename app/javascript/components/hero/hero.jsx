ab39b8638c811f84d41421e7940e12630623f347
Keiran <keiranhoward.dev@gmail.com>
2021-01-30 00:36:39 +1100

    added react styling for hero background

--- app/javascript/components/hero/hero.jsx
+++ app/javascript/components/hero/hero.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import './hero.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import background from 'images/index-background.jpg'
 
+const styles = {
+    imageContainer: {
+        height: '100vh',
+        width: '100vw',
+        backgroundSize: 'cover',
+        backgroundPositionY: '60%',
+        overflow: 'hidden',
+        backgroundRepeat:'no-repeat',
+        backgroundImage: `url(${background})`
+
+    }
+};
 
 
 const Hero = () => {
   return (
-    <header className="masthead">
-    <div className="container-fluid h-100">
-      <div className="row h-100 align-items-center">
-        <div className="col-12 text-center">
-          <h1 className="font-weight-light">Vertically Centered Masthead Content</h1>
-          <p className="lead">A great starter layout for a landing page</p>
-        </div>
-        <div className="col-sm-3">
-            <a href="#" className="btn btn-lg">button</a>
+    <div className='container-fluid' style={styles.imageContainer}>
+      
+      <div className="p-5 text-center">
+          <div className="d-flex justify-content-center align-items-center h-100 ">
+            <div className="text-white">
+              <h1 className="mb-3">Heading</h1>
+              <h4 className="mb-3">Subheading</h4>
+              <a className="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
+            </div>
         </div>
       </div>
     </div>
-  </header>
-
   );
 };
 
