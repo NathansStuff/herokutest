ede683557b7147625d0e94a9cf3890fa2d651c3a
Jackson Chaffey <jacksonchaffey@gmail.com>
2021-01-30 19:06:23 +1100

    added user model, sessions and regos controller, added routing for both

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create]
+  resources :registraions, only: [:create]
   root 'home#index'
 
   namespace :api do
