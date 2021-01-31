672793bee852bf668e9c8e283b7781002af0d2c7
Jackson Chaffey <jacksonchaffey@gmail.com>
2021-02-01 09:56:49 +1100

    changed app.js to a class component

--- app/javascript/routing/App.js
+++ app/javascript/routing/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Animals from '../pages/animals/animals';
 import Animal from '../pages/animal/animal';
@@ -7,21 +7,23 @@ import HomePage from '../pages/home/home';
 import Login from '../pages/login/login';
 import Signup from '../pages/signup/signup';
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/animal/:id' component={Animal} />
-          <Route exact path='/contact' component={Contact} />
-          <Route exact path='/animals' component={Animals} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={Signup} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+export default class App extends Component {
+  render(){
+    return (
+     <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/animal/:id' component={Animal} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/animals' component={Animals} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 };
 
-export default App;
+
