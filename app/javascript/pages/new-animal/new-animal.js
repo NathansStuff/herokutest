83fbb890f442bd82f86fce09495085cbe073f558
Nathanstuff <n.odonnell@hotmail.com>
2021-01-31 16:54:06 +1100

    new animal form created

--- app/javascript/pages/new-animal/new-animal.js
+++ app/javascript/pages/new-animal/new-animal.js
@@ -1,8 +1,47 @@
-import React from 'react'
-import './new-animal.scss'
+import React from 'react';
+import './new-animal.scss';
 
 const NewAnimal = () => {
-    return <div>New animal page</div>
-}
+  return (
+    <div className='new-animal-wrapper'>
+      <div className='new-animal-form'>
+        <div className='new-animal-title'>
+          <h3>New Animal</h3>
+        </div>
+        <form>
+          <div className='new-animal-field'>
+            <input name='name' placeholder='Name'></input>
+          </div>
+          <div className='new-animal-field'>
+            <input name='age' type='number' placeholder='Age'></input>
+          </div>
+          <div className='new-animal-field'>
+            <input name='breed' placeholder='Breed'></input>
+          </div>
+          <div className='new-animal-field'>
+            <input
+              name='microchip_number'
+              type='number'
+              placeholder='Microchip Number'
+            ></input>
+          </div>
+          <div className='new-animal-field'>
+            <textarea
+              name='notes'
+              rows='10'
+              cols='50'
+              placeholder='Notes: 
+            
+            eg. Medication, Desexed date'
+            ></textarea>
+          </div>
+          <div>
+            <button type='submit'>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default NewAnimal
+export default NewAnimal;
