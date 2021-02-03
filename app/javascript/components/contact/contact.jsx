0f8de6d364f1bf8e44d35d6ddcb576eeebaa7c35
Nathanstuff <n.odonnell@hotmail.com>
2021-02-03 16:07:22 +1100

    home#contact sends email

--- app/javascript/components/contact/contact.jsx
+++ app/javascript/components/contact/contact.jsx
@@ -1,42 +1,118 @@
-import React from 'react';
-import './contact.scss'
+import React, { Component } from 'react';
+import './contact.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import * as emailjs from 'emailjs-com';
+import {
+  Button
+} from 'reactstrap';
 
+class Contact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  };
 
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state)
+    const { name, email, phone, message } = this.state;
+    let templateParams = {
+      from_name: email,
+      to_name: 'cybeleproject93@gmail.com',
+      phone: phone,
+      message: message,
+    };
+    emailjs
+      .send(
+        'service_oj6ecd1',
+        'template_wb5x5je',
+        templateParams,
+        'user_vTT2nk4pHgmYk78U5flbL'
+      )
+      .then(result => {
+        console.log(result);
+      });
+    this.resetForm();
+  }
+  resetForm() {
+    this.setState({
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    });
+  }
 
-const Contact = () => {
-  return (
-    
-      <div className="container contact-form ">
-      <form method="post">
+  handleChange = (param, e) => {
+    this.setState({ [param]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className='container contact-form '>
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <h3>Have an idea for a feature? let us know</h3>
-        <div className="row">
-              <div className="col-md-6">
-                  <div className="form-group">
-                      <input type="text" name="txtName" className="form-control" placeholder="Name *" />
-                  </div>
-                  <div className="form-group">
-                      <input type="text" name="txtEmail" className="form-control" placeholder="Email *" />
-                  </div>
-                  <div className="form-group">
-                      <input type="text" name="txtPhone" className="form-control" placeholder="Phone Number *" />
-                  </div>
-                  <div className="form-group">
-                      <input type="submit" name="btnSubmit" className="btnContact" />
-                  </div>
+          <div className='row'>
+            <div className='col-md-6'>
+              <div className='form-group'>
+                <input
+                  type='text'
+                  name='txtName'
+                  className='form-control'
+                  placeholder='Name *'
+                  value={this.state.name}
+                  onChange={this.handleChange.bind(this, 'name')}
+                />
               </div>
-              <div className="col-md-6">
-                  <div className="form-group">
-                      <textarea name="txtMsg" className="form-control" placeholder="Message" ></textarea>
-                  </div>
+              <div className='form-group'>
+                <input
+                  type='text'
+                  name='txtEmail'
+                  className='form-control'
+                  placeholder='Email *'
+                  value={this.state.email}
+                  onChange={this.handleChange.bind(this, 'email')}
+                />
               </div>
+              <div className='form-group'>
+                <input
+                  type='text'
+                  name='txtPhone'
+                  className='form-control'
+                  placeholder='Phone Number *'
+                  value={this.state.phone}
+                  onChange={this.handleChange.bind(this, 'phone')}
+                />
+              </div>
+              <Button className='form-group' type='submit'>
+                send
+              </Button>
+            </div>
+            <div className='col-md-6'>
+              <div className='form-group'>
+                <textarea
+                  name='txtMsg'
+                  className='form-control'
+                  placeholder='Message'
+                  value={this.state.message}
+                  onChange={this.handleChange.bind(this, 'message')}
+                ></textarea>
+              </div>
+            </div>
           </div>
-      </form>
-  </div>
-
-  )
-};
-
+        </form>
+      </div>
+    );
+  }
+}
 export default Contact;
+
+/*
+              <Button className='form-group'>
+                <input type='submit' name='btnSubmit' className='btnContact' />
+              </Button>
+              */
--- app/javascript/pages/animals/animals.jsx
+++ app/javascript/pages/animals/animals.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import AnimalCard from '../../components/animal-card/animal-card'
+import AnimalCard from '../../components/animal-card/animal-card';
 
 const Animals = () => {
   const [animals, setAnimals] = useState([]);
@@ -14,99 +14,19 @@ const Animals = () => {
     axios
       .get('./api/v1/animals.json')
       .then(resp => {
-        setAnimals(resp.data.data)
+        setAnimals(resp.data.data);
       })
       .catch(resp => console.log(resp), [animals.length]);
   }, []);
 
-  const list = animals.map( animal => {
-      return (
-        <AnimalCard
-          key={animal.attributes.name}
-          attributes={animal}
-        />
-        );
-  })
+  const list = animals.map(animal => {
+    return <AnimalCard key={animal.attributes.name} attributes={animal} />;
+  });
   return (
-      <Fragment>
-        <div classNameName="container-fluid row Profile">
-          <div classNameName="col-md-4 ">
-            <div classNameName="profile-img">
-              <img src="" alt=""/>
-              <p classNameName="display4">Example Name</p>
-            </div>
-            <div classNameName="row">
-              <div classNameName="col-md-4">
-                <div classNameName="profile-info">
-                  <p>test</p>
-                  <br/>
-                  <p>test</p>
-                  <br/>
-                  <p>test</p>
-                  <br/>
-                  <p>test</p>
-                  <br/>
-                  <p>test</p>
-                  <br/>
-                  <p>test</p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-          <div className="col-md-8">
-                        <div className="tab-content profile-tab" id="myTabContent">
-                            <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                                <label>User Id</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p>Test123</p>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                                <label>Name</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p>Test Test</p>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                                <label>Email</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p>test@gmail.com</p>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                                <label>Phone</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p>123 456 7890</p>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                                <label>Profession</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p>Web Developer and Designer</p>
-                                            </div>
-                                        </div>
-                            </div>
-                          
-                        </div>
-                    </div>
-        
-          <ul>{list}</ul>
-        </div>
-        
-      </Fragment>
-  )
+    <Fragment>
+      <ul>{list}</ul>
+    </Fragment>
+  );
 };
 
 export default Animals;
