a405f03b98cf26ec46fc8ff7027dded083cf62ee
Nathanstuff <n.odonnell@hotmail.com>
2021-02-05 22:26:21 +1100

    animal model has_one_attached :photo

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,4 +3,5 @@ class Animal < ApplicationRecord
     validates :name, presence: true
     validates :age, numericality: { greater_than: 0}, presence: true
     validates :breed, presence: true
+    has_one_attribute :photo
 end
