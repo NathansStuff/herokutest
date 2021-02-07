4228d888c694ce03e75ffa6246505426c9ae65a5
Nathanstuff <n.odonnell@hotmail.com>
2021-02-07 14:15:21 +1100

    setup env keys

--- app/javascript/aws/config.js
+++ app/javascript/aws/config.js
@@ -1,10 +1,21 @@
-import aws from '../../../keys';
+// import aws from '../../../keys';
+
+// const config = {
+//   bucketName: aws.bucket,
+//   region: aws.region,
+//   accessKeyId: aws.access_key_id,
+//   secretAccessKey: aws.secret_access_key,
+//   header: 'Access-Control-Allow-Origin',
+// };
+
+// export default config;
+
 
 const config = {
-  bucketName: aws.bucket,
-  region: aws.region,
-  accessKeyId: aws.access_key_id,
-  secretAccessKey: aws.secret_access_key,
+  bucketName: ENV.BUCKET_NAME,
+  region: ENV.REGION,
+  accessKeyId: ENV.ACCESS_KEY,
+  secretAccessKey: ENV.SECRET_ACCESS_KEY,
   header: 'Access-Control-Allow-Origin',
 };
 
