67c95d89cc0348926c016c630d50c1689e3a8bd8
Keiran <keiranhoward.dev@gmail.com>
2021-02-08 20:32:43 +1100

    fixed auth for merge

--- app/javascript/aws/config.js
+++ app/javascript/aws/config.js
@@ -11,12 +11,6 @@
 // export default config;
 
 
-const config = {
-  bucketName: 'cybeleproject-bucket-dev',
-  region: 'us-east-2',
-  accessKeyId: 'AKIAV2MJKGCLSK2EHGGV',
-  secretAccessKey: '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',
-  header: 'Access-Control-Allow-Origin',
-};
+
 
 export default config;
