91777ee55d98c91f689c980c64376d5f2f6ebb87
Nathanstuff <n.odonnell@hotmail.com>
2021-02-09 18:11:57 +1100

    fixed erroneous imports on about component

--- app/javascript/components/about/about.jsx
+++ app/javascript/components/about/about.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import background from 'images/aboutbackground.jpg';
-import adopt from 'images/rescue.jpg';
-import pets from 'images/dogcat.jpg'
+import background from '../../../assets/images/aboutbackground.jpg';
+import adopt from '../../../assets/images/rescue.jpg';
+import pets from '../../../assets/images/dogcat.jpg'
 
 const styles = {
   imageContainer: {
