9c35f39797612a255f37c856fa967a4b4847a61d
Nathanstuff <n.odonnell@hotmail.com>
2021-02-09 18:04:21 +1100

    about section details changed from laurem ipsum

--- app/javascript/components/about/about.jsx
+++ app/javascript/components/about/about.jsx
@@ -26,7 +26,7 @@ const About = () => {
       <div className="row align-items-center h-100 p-5">
         <div className="col-lg-5 order-2 order-lg-1"><i className="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
           <h2 className="display-6 text-white">What is The Cybele Project?</h2>
-          <p className="font-italic text-white mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className="font-italic text-white mb-4">An application for rescue animals carers to track their charges care.</p>
         </div>
         <div className="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src={pets} alt="" className="img-fluid  mb-lg-0 pets"/></div>
       </div>
@@ -35,7 +35,7 @@ const About = () => {
         <div className="col-lg-5 px-5 mx-auto"><img src={adopt} alt="" className="img-fluid mb-4 mb-lg-0 adopt"/></div>
         <div className="col-lg-6"><i className="fa fa-leaf fa-2x mb-3 text-primary"></i>
           <h2 className="display-6 text-white">Adoptions</h2>
-          <p className="font-italic text-white mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><a href="#" className="btn bg-info text-white px-3 rounded-pill shadow-sm">Learn More</a>
+          <p className="font-italic text-white mb-4">If you are interested in adopting one of our animals, please send us an email using the contact form below.</p><a href="#" className="btn bg-info text-white px-3 rounded-pill shadow-sm">Learn More</a>
         </div>
       </div>
     </div>
